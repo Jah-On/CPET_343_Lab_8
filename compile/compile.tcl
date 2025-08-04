cd260d863dd1eace121cffe5bd7d275ddf8759b8
John Schulz <john.schulz1@protonmail.com>
2025-08-04 11:35:29 -0400

    Update compilation and programming files.

--- compile/compile.tcl
+++ compile/compile.tcl
@@ -2,12 +2,12 @@
 # Quartus II compile script for DE1-SoC board
 
 # 1] name your project here
-set project_name "Lab_7_TLDE"
+set project_name "Lab_8_TLDE"
 
-file delete -force ../project
-file delete -force ../output_files
-file mkdir ../project
-cd ../project
+file delete -force project
+file delete -force output_files
+file mkdir project
+cd project
 load_package flow
 project_new $project_name
 set_global_assignment -name VHDL_INPUT_VERSION VHDL_2008
@@ -17,33 +17,25 @@ set_global_assignment -name TOP_LEVEL_ENTITY tlde
 set_global_assignment -name PROJECT_OUTPUT_DIRECTORY ../output_files
 
 # 2] include your relative path files here
-set_global_assignment -name VHDL_FILE ../src/synchronizers/rising_edge_synchronizer.vhd
-set_global_assignment -name VHDL_FILE ../src/synchronizers/clock_synchronizer.vhd
-set_global_assignment -name VHDL_FILE ../src/synchronizers/package_def.vhd
-set_global_assignment -name VHDL_FILE ../src/alu.vhd
-set_global_assignment -name VHDL_FILE ../src/memory.vhd
-set_global_assignment -name VHDL_FILE ../src/fsm.vhd
-set_global_assignment -name VHDL_FILE ../src/ssd_d3.vhd
-set_global_assignment -name VHDL_FILE ../src/tlde.vhd
+set_global_assignment -name SEARCH_PATH   "../.."
+set_global_assignment -name VHDL_LMF_FILE ../maxplus2.lmf
+set_global_assignment -name  QIP_FILE     src/rom/rom.qip
+set_global_assignment -name VHDL_FILE     src/rom/rom.vhd
+set_global_assignment -name VHDL_FILE     src/synchronizers/clock_synchronizer.vhd
+set_global_assignment -name VHDL_FILE     src/synchronizers/rising_edge_synchronizer.vhd
+set_global_assignment -name VHDL_FILE     src/synchronizers/package_def.vhd
+set_global_assignment -name VHDL_FILE     src/alu.vhd
+set_global_assignment -name VHDL_FILE     src/memory.vhd
+set_global_assignment -name VHDL_FILE     src/ssd_d3.vhd
+set_global_assignment -name VHDL_FILE     src/fsm.vhd
+set_global_assignment -name VHDL_FILE     src/tlde.vhd
+
+# 3] Assign pins
 
 set_location_assignment PIN_AF14 -to clk
 
 set_location_assignment PIN_AA14 -to reset
-set_location_assignment PIN_AA15 -to mr
-set_location_assignment PIN_W15  -to ms
 set_location_assignment PIN_Y16  -to exec
- 
-set_location_assignment PIN_AB12 -to number[0]
-set_location_assignment PIN_AC12 -to number[1]
-set_location_assignment PIN_AF9  -to number[2]
-set_location_assignment PIN_AF10 -to number[3]
-set_location_assignment PIN_AD11 -to number[4]
-set_location_assignment PIN_AD12 -to number[5]
-set_location_assignment PIN_AE11 -to number[6]
-set_location_assignment PIN_AC9  -to number[7]
-
-set_location_assignment PIN_AD10 -to op[0]
-set_location_assignment PIN_AE12 -to op[1]
 
 set_location_assignment PIN_AE26 -to hex0[0]
 set_location_assignment PIN_AE27 -to hex0[1]
---
+++ compile/program.sh
@@ -0,0 +1,1 @@
+quartus_pgm --mode=JTAG -o "P;output_files/Lab_8_TLDE.sof@2"
